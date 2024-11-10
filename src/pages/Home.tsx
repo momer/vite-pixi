103bf35eca0dfbaeed0a5546b63b0668c09d7ced
Mo Omer <mo@ipv7.com>
2024-11-10 11:11:12 -0600

    ok it sort of works, the animation in the background doesnt

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,10 +15,11 @@ import logoPhobiaDark from '/static/images/clients/phobia/logo-dark.svg';
 import logoPhobiaLight from '/static/images/clients/phobia/logo-light.svg';
 import logoUnseal from '/static/images/clients/unseal/logo-light.svg';
 import imageLaptop from '/static/images/laptop.jpg';
-import { Helmet } from 'react-helmet';
 import { useEffect, useState } from 'react';
 import { StringMap } from '@/lib/stringMap';
 import { CaseStudy, MDXEntry } from '@/lib/mdx';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
 
 const clients = [
     ['Phobia', logoPhobiaLight],
@@ -189,40 +190,42 @@ export function Home() {
 
     return (
         <>
-            <Helmet>
-                <title>DCEK -&gt;&lt;- Systems Thinking in Bloom</title>
-                <meta name="description"
+            <HelmetProvider>
+                <Helmet>
+                    <title>DCEK -&gt;&lt;- Systems Thinking in Bloom</title>
+                    <meta name="description"
                       content="We're an engineering and systems design agency, bringing technology products to our clients customers."/>
-            </Helmet>
-            <Container className="mt-24 sm:mt-32 md:mt-56">
-                <FadeIn className="max-w-3xl">
-                    <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-                        Award-winning development studio based in Denmark.
-                    </h1>
-                    <p className="mt-6 text-xl text-neutral-600">
-                        We are a development studio working at the intersection of design
-                        and technology. It’s a really busy intersection though — a lot of
-                        our staff have been involved in hit and runs.
-                    </p>
-                </FadeIn>
-            </Container>
+                </Helmet>
+                <Container className="mt-24 sm:mt-32 md:mt-56">
+                    <FadeIn className="max-w-3xl">
+                        <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
+                            Award-winning development studio based in Denmark.
+                        </h1>
+                        <p className="mt-6 text-xl text-neutral-600">
+                            We are a development studio working at the intersection of design
+                            and technology. It’s a really busy intersection though — a lot of
+                            our staff have been involved in hit and runs.
+                        </p>
+                    </FadeIn>
+                </Container>
 
-            <Clients/>
+                <Clients/>
 
-            <CaseStudies caseStudies={ caseStudies }/>
+                <CaseStudies caseStudies={ caseStudies }/>
 
-            <Testimonial
+                <Testimonial
                 className="mt-24 sm:mt-32 lg:mt-40"
                 client={ { name: 'Phobia', logo: logoPhobiaDark } }
             >
-                The team at Studio went above and beyond with our onboarding, even
-                finding a way to access the user’s microphone without triggering one of
-                those annoying permission dialogs.
-            </Testimonial>
+                    The team at Studio went above and beyond with our onboarding, even
+                    finding a way to access the user’s microphone without triggering one of
+                    those annoying permission dialogs.
+                </Testimonial>
 
-            <Services/>
+                <Services/>
 
-            <ContactSection/>
+                <ContactSection/>
+            </HelmetProvider>
         </>
     );
 }
