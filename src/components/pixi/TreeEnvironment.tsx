c82d289317c1fffe2e10bd06cce7cc0ab241e398
Mo Omer <mo@ipv7.com>
2024-12-02 14:46:42 -0600

    ok, tree env container has no width, need to fix that and then patch the updateOwnerPos callback before updateEmitter

--- src/components/pixi/TreeEnvironment.tsx
+++ src/components/pixi/TreeEnvironment.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, MutableRefObject, useCallback, useEffect, useRef, useState } from 'react';
+import { forwardRef, MutableRefObject, RefObject, useCallback, useEffect, useRef, useState } from 'react';
 
 import hardRainImageUrl from '/static/images/pixi/sakura/environment/HardRain.png';
 import { Container } from 'pixi.js';
@@ -10,81 +10,83 @@ extend({
     Container
 });
 
-const hardRainConfig = {
-    'lifetime': {
-        'min': 0.81,
-        'max': 0.81
-    },
-    'frequency': 0.004,
-    'emitterLifetime': 0,
-    'maxParticles': 1000,
-    'addAtBack': false,
-    'pos': {
-        'x': 0,
-        'y': 0
-    },
-    'behaviors': [
-        {
-            'type': 'alphaStatic',
-            'config': {
-                'alpha': 0.5
-            }
-        },
-        {
-            'type': 'moveSpeedStatic',
-            'config': {
-                'min': 3000,
-                'max': 3000
-            }
-        },
-        {
-            'type': 'scaleStatic',
-            'config': {
-                'min': 1,
-                'max': 1
-            }
-        },
-        {
-            'type': 'rotationStatic',
-            'config': {
-                'min': 65,
-                'max': 65
-            }
-        },
-        {
-            'type': 'textureRandom',
-            'config': {
-                'textures': [
-                    'hardRain',
-                ]
-            }
-        },
-        {
-            'type': 'spawnShape',
-            'config': {
-                'type': 'rect',
-                'data': {
-                    'x': 0,
-                    'y': 0,
-                    'w': 900,
-                    'h': 20
-                }
-            }
-        }
-    ]
-};
+
 
 
 export interface TreeEnvironmentProps {
     children?: React.ReactNode;
 }
 
-export const TreeEnvironment = forwardRef<Tree, TreeEnvironmentProps>((props, ref) => {
+export const TreeEnvironment = forwardRef<RefObject<Tree>, TreeEnvironmentProps>((props, ref) => {
     const environmentContainerRef: MutableRefObject<Container | null> = useRef<Container>(null);
     const emitterRef: MutableRefObject<Emitter | null> = useRef<Emitter>(null);
     const [isEnvironmentContainerLoading, setIsEnvironmentContainerLoading] = useState(true);
     const [precipitationParticleCount, setPrecipitationParticleCount] = useState(500);
     const { treeWorldContainerRef } = ref as unknown as Tree;
+
+    const precipConfigRef: MutableRefObject<any> = useRef({
+        'lifetime': {
+            'min': 0.81,
+            'max': 0.81
+        },
+        'frequency': 0.004,
+        'emitterLifetime': 0,
+        'maxParticles': 1000,
+        'addAtBack': false,
+        'pos': {
+            'x': 0,
+            'y': 0
+        },
+        'behaviors': [
+            {
+                'type': 'alphaStatic',
+                'config': {
+                    'alpha': 0.5
+                }
+            },
+            {
+                'type': 'moveSpeedStatic',
+                'config': {
+                    'min': 3000,
+                    'max': 3000
+                }
+            },
+            {
+                'type': 'scaleStatic',
+                'config': {
+                    'min': 1,
+                    'max': 1
+                }
+            },
+            {
+                'type': 'rotationStatic',
+                'config': {
+                    'min': 65,
+                    'max': 65
+                }
+            },
+            {
+                'type': 'textureRandom',
+                'config': {
+                    'textures': [
+                        'hardRain',
+                    ]
+                }
+            },
+            {
+                'type': 'spawnShape',
+                'config': {
+                    'type': 'rect',
+                    'data': {
+                        'x': 0,
+                        'y': 0,
+                        'w': ref?.current?.treeObjectContainerRef?.width,
+                        'h': 20
+                    }
+                }
+            }
+        ]
+    });
 
     const handleParticleContainer = useCallback((container: Container) => {
         environmentContainerRef.current = container;
@@ -138,12 +140,12 @@ export const TreeEnvironment = forwardRef<Tree, TreeEnvironmentProps>((props, re
 
     useEffect(() => {
         // TODO: change based on envirnment settings, like rain==true, etc.
-        if (!isEnvironmentContainerLoading && environmentContainerRef?.current !== null && assetLoadSuccess && hardRain && isSuccess) {
+        if (!isEnvironmentContainerLoading && precipConfigRef?.current && environmentContainerRef?.current !== null && assetLoadSuccess && hardRain && isSuccess) {
             emitterRef.current = new Emitter(
                 environmentContainerRef.current,
-                hardRainConfig,
+                precipConfigRef.current,
             );
-            emitterRef.current.updateOwnerPos(window.innerWidth / 2, window.innerHeight / 2);
+            // emitterRef.current.updateOwnerPos(window.innerWidth / 2, window.innerHeight / 2);
 
             updateEmitter();
         }
