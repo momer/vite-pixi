8a0d8615f15f2851396c78ce067b3a726170e13a
Mo Omer <mo@ipv7.com>
2024-12-26 07:05:52 -0600

    adjust offset of tree

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,77 +12,15 @@ import bonsaiLogo from '/static/images/clients/bonsai/bonsai.png';
 import astellasLogo from '/static/images/clients/astellas/astellas.png';
 import mHUBLogo from '/static/images/clients/mHUB/mHUB.png';
 import twinmoLogo from '/static/images/clients/twinmo/twinmo-logo.png';
-import cranesUPickLogo from '/static/images/clients/cranes/cranes-u-pick-logo.png';
 
 import React, { useCallback, useId, useRef, useState } from 'react';
-// import { StringMap } from '@/lib/stringMap';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import clsx from 'clsx';
 import { HomeDemo } from '@/components/pixi/HomeDemo';
 import { TreeConfigurator } from '@/components/pixi/TreeConfigurator';
 import { TreeProvider } from '@/components/pixi/TreeProvider';
 import { PlainImage } from '@/components/PlainImage';
-import { ParallaxAny } from '@/components/ParallaxAny';
 import { InteractiveMarquee } from '@/components/Marquee';
-
-interface StringMap {
-    [key: string]: string;
-}
-
-function Services() {
-    return (
-        <>
-            <SectionIntro
-                eyebrow="Services"
-                title="We help you identify, explore and respond to new opportunities."
-                className="mt-24 sm:mt-32 lg:mt-40"
-            >
-                <p>
-                    As long as those opportunities involve giving us money to re-purpose
-                    old projects — we can come up with an endless number of those.
-                </p>
-            </SectionIntro>
-            <Container className="mt-16">
-                <div className="lg:flex lg:items-center lg:justify-end">
-                    <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
-                        <FadeIn className="w-[33.75rem] flex-none lg:w-[45rem]">
-                            <StylizedImage
-                                sizes="(min-width: 1024px) 41rem, 31rem"
-                                className="justify-center lg:justify-end"
-                            />
-                        </FadeIn>
-                    </div>
-                    <List className="mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
-                        <ListItem title="Web development">
-                            We specialise in crafting beautiful, high quality marketing pages.
-                            The rest of the website will be a shell that uses lorem ipsum
-                            everywhere.
-                        </ListItem>
-                        <ListItem title="Application development">
-                            We have a team of skilled developers who are experts in the latest
-                            app frameworks, like Angular 1 and Google Web Toolkit.
-                        </ListItem>
-                        <ListItem title="E-commerce">
-                            We are at the forefront of modern e-commerce development. Which
-                            mainly means adding your logo to the Shopify store template we’ve
-                            used for the past six years.
-                        </ListItem>
-                        <ListItem title="Custom content management">
-                            At Studio we understand the importance of having a robust and
-                            customised CMS. That’s why we run all of our client projects out
-                            of a single, enormous Joomla instance.
-                        </ListItem>
-                    </List>
-                </div>
-            </Container>
-        </>
-    );
-}
-
-export const metadata: StringMap = {
-    title: 'Contact Us',
-    description: 'Let’s work together. We can’t wait to hear from you.',
-};
 
 export function Home() {
     const fullsizeDivRef = useRef<HTMLDivElement | null>(null);
@@ -151,7 +89,7 @@ export function Home() {
 
                     </div>
 
-                    <div id="pixi-app" className={ clsx('top-12 left-0 m-0 p-0 absolute touch-none z-10') }>
+                    <div id="pixi-app" className={ clsx('top-16 left-0 m-0 p-0 absolute touch-none z-10') }>
                         { <HomeDemo ref={ fullsizeDivRef }/> }
                     </div>
 
@@ -364,11 +302,6 @@ export function Home() {
                             </div>
                         </div>
                     </div>
-
-                    <Services/>
-
-                    <ContactSection/>
-
                 </TreeProvider>
             </HelmetProvider>
         </>
