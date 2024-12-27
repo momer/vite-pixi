87d007bfde4e161780c5c25097debf0821e55ad5
Mo Omer <mo@ipv7.com>
2024-12-27 16:06:26 -0600

    adjust container width

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -15,7 +15,7 @@ export function Container<T extends React.ElementType = 'div'>({
   const Component = as ?? 'div';
 
   return (
-      <Component className={clsx('mx-auto max-w-7xl px-6 lg:px-8', className)}>
+      <Component className={clsx('mx-auto sm:px-2 md:px-4', className)}>
           <div className="mx-auto max-w-2xl lg:max-w-none">{children}</div>
       </Component>
   );
